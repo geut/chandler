06554bfde8257c8a41aa29d82292ca56f5017097
Maximiliano Fierro <elmasse@gmail.com>
2016-09-26 18:05:55 -0300

    first working app

---
+++ main/index.js
@@ -0,0 +1,22 @@
+
+import path from 'path';
+
+import { app, BrowserWindow } from 'electron';
+
+const index = path.resolve(__dirname, '..', 'index.html');
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+console.log(`file://${index}`)
+  mainWindow.loadURL(`file://${index}`);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});

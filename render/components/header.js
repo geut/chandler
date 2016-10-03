33f26ceacd7d60855ad245a69b30203319141a10
Maximiliano Fierro <elmasse@gmail.com>
2016-10-03 20:50:32 -0300

    style changes

--- render/components/header.js
+++ render/components/header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   
   render() {
     const { project } = this.props;
-    const { lighter, toolbar } = styles;
+    const { toolbar, lighter, folder } = styles;
 
     const { name, path, version } = project;
   
@@ -17,9 +17,17 @@ export default class Header extends Component {
         <h1>
           {name} <span className={css(lighter)}> v{version}</span>            
         </h1>
-        : <em>No Project Selected</em> 
-       }
-       { path ? <p><i className="fa fa-folder-open-o"/>{path}</p> : <span /> }
+        :
+        <em>No Project Selected</em> 
+      }
+      { 
+        path ?
+        <p className={css(folder)}>
+          <i className="fa fa-folder-open-o"/>{path}
+        </p> 
+        : 
+        <span /> 
+      }
       </header>
     );
   }
@@ -31,7 +39,16 @@ const styles = StyleSheet.create({
   },
   toolbar: {
     borderBottom: '1px solid #ccc',
-    minHeight: 70
+    minHeight: 60,
+    position: 'relative'
+  },
+  folder: {
+    fontSize: 12,
+    position: 'absolute',
+    top: -25,
+    left: 5,
+    color: '#333',
+    fontWeight: 200    
   }
   
 });

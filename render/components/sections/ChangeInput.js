db1127027959d602e07334db8edc9755fce1b1c4
Max Fierro <elmasse@gmail.com>
2017-02-16 17:50:35 -0300

    refactored Input into its own Component

---
+++ render/components/sections/ChangeInput.js
@@ -0,0 +1,51 @@
+
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+export default class ChangeInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleSave = (e) => {
+    const { onSave, kind } = this.props;
+    const { text } = this.state;
+    onSave(kind, text);
+    this.setState({ text: '' });
+  }
+
+  handleCancel = (e) => {
+    const { onCancel } = this.props;
+    onCancel();
+    this.setState({ text: '' });
+  }
+
+  handleChange = (e) => {
+    this.setState({text: e.target.value});
+  }
+
+  render() {
+    const { editing, kind } = this.props;
+    const { text } = this.state;
+
+    const { actions } = styles;
+
+    return (
+      <div hidden={kind && editing !== kind }>
+        <input type="text"
+          onChange={this.handleChange}
+          value={text}
+        />
+        <div className={css(actions)}>
+          <button type="button" onClick={this.handleSave}>Add</button>
+          <button type="button" onClick={this.handleCancel}>Cancel</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  actions: {}
+})
